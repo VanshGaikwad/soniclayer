ff7c201d4c76fe82e95a3f69537a241b34851c9d
vansh gaikwad <vanshgaikwad72@gmail.com>
2026-02-27 15:49:38 +0530

    prefix for api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 // middle-ware
 app.use(express.json());
 app.use(cookieParser());
-
+// prefix for api
+app.use('./api/auth',authRoutes);
 module.exports = app;
